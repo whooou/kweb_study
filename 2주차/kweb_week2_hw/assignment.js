3f5d1f37a3555e834a0e4c31c82c0cb09e24bc02
whooou <eswb37v@naver.com>
2018-10-12 05:42:07 +0900

    promise to async/await

--- 2주차/kweb_week2_hw/assignment.js
+++ 2주차/kweb_week2_hw/assignment.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const util = require('util');
 
 /*initial code*/
 /*
@@ -22,7 +23,7 @@ fs.writeFile('./1.txt', '11111', function(err) {
 
 
 /*Promise*/
-
+/*
 function readFile(fileName, type){
     return new Promise((resolve, reject)=>{
         fs.readFile(fileName,type, (err,data)=>{
@@ -59,3 +60,26 @@ writeFile('./1.txt', '11111')
     .then((data3)=>{
         console.log(data3);
     })
+*/
+
+
+/* async / await */
+
+const fs_readFile = util.promisify(fs.readFile);
+const fs_writeFile = util.promisify(fs.writeFile);
+
+async function test() {
+    await fs_writeFile('./1.txt', '11111');
+    await console.log('1.txt created');
+    const data1 = await fs_readFile('./1.txt','UTF-8');
+    await fs_writeFile('./2.txt',data1 + '22222');
+    await console.log('2.txt created');
+    const data2 = await fs_readFile('./2.txt','UTF-8')
+    await fs_writeFile('./3.txt',data2 + '33333');
+    await console.log('3.txt created');
+    const data3 = await fs_readFile('./3.txt','UTF-8');
+    await console.log(data3);
+}
+
+test();
+
